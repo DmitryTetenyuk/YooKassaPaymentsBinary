92123f55e266bab78fd31fa69f235eb62e4cadd6
Dmitry Tetenyuk <dmitrytetenyuk@ya.ru>
2022-06-23 15:35:47 +0300

    Add binary

--- Package.swift
+++ Package.swift
@@ -11,13 +11,17 @@ let package = Package(
             type: .static,
             targets: ["YooKassaPaymentsBinary"]),
     ],
-    dependencies: [
-    ],
     targets: [
+        .binaryTarget(
+            name: "YooKassaPayments",
+            url: "https://github.com/DmitryTetenyuk/YooKassaPaymentsBinary/releases/download/6.8.0/YooKassaPayments.xcframework.zip",
+            checksum: "0e80647fac0cf85414ac040ebecdb7c35df098955335b41e43d88c6f74c15ce5"
+        ),
         .target(
             name: "YooKassaPaymentsBinary",
             dependencies: [
-                "YandexMapsMobile"
-            ],
+                "YooKassaPayments"
+            ]
+        ),
     ]
 )
